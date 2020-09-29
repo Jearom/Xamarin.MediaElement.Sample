d9333bcda84d643e83eb8ae684501ade359463dc
Barış YILDIRIM <barisy@biscozum.com.tr>
2020-09-29 15:01:11 +0300

    Add project files.

---
+++ Xamarin.MediaElement.Sample/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.MediaElement.Sample.Models;
+
+namespace Xamarin.MediaElement.Sample.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
